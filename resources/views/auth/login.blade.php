8bcc6ff7dd0d19621fbd9386bc0cd17576e9ca6f
BrenoJalmir <50642258+BrenoJalmir@users.noreply.github.com>
2021-12-10 20:21:01 -0300

    added register and login functions

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends ('layouts.app')
+
+@section('content')
+<div style="height: 460px;" class="d-flex justify-content-center vh-50">
+    <div style="height: 300px" class="w-50 bg-light align-self-center shadow rounded">
+        <form action="/login" method="POST">
+            @csrf
+            <div class="container d-flex flex-column vstack gap-4 px-5">
+                <div class="form-group row mt-5">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10 ">
+                        <input name="email" type="email" class="form-control" placeholder="email@example.com">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Senha</label>
+                    <div class="col-sm-10">
+                        <input name="password" type="password" class="form-control" placeholder="Senha">
+                    </div>
+                </div>
+                <input class="btn-contribua align-self-center" type="submit">
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
         @livewireStyles
 
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
     </head>
     <body>
         @section('navbar')
