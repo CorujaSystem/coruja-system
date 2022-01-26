58f2de00589395972d33d4e65914ce2a63600a6e
NovaDrake76 <novadrake76@gmail.com>
2022-01-26 19:42:59 -0300

    student register progress

--- resources/views/livewire/student-form.blade.php
+++ resources/views/livewire/student-form.blade.php
@@ -1,34 +1,33 @@
 <div class="d-flex flex-column align-items-center">
-    <p>{{$studentCount}}</p>
-
-    <button wire:click="$emit('openModal', 'student-modal') class="form-control w-50 align-self-center mb-4 "> Enviar </button>
+    <!-- <p> Total de alunos: {{$studentCount}}</p>
+ -->
 
     <div>
-    <table class="table mt-4">
-        <thread>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Telefone</th>
-                <th scope="col">Genero</th>
-                <th scope="col">Responsável</th>
-                <th scope="col">observação</th>
-        </thread>
-        <tbody>
-        @foreach ($students as $s)
+        <table class=" table mt-4">
+            <thread>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Sexo</th>
+                    <th scope="col">Responsável</th>
+                    <th scope="col">observação</th>
+            </thread>
+            <tbody>
+                @foreach ($students as $s)
 
-            <tr>
-                <th scope="row">{{$s['id']}}</th>
-                <th scope="row">{{$s['name']}}</th>
-                <th scope="row">{{$s['tel']}}</th>
-                <th scope="row">{{$s['gender']}}</th>
-                <th scope="row">{{$s['responsible']}}</th>
-                <th scope="row">{{$s['observation']}}</th>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+                <tr>
+                    <th scope="row">{{$s['name']}}</th>
+                    <th scope="row">{{$s['tel']}}</th>
+                    <th scope="row">{{$s['gender']}}</th>
+                    <th scope="row">{{$s['responsible']}}</th>
+                    <th scope="row">{{$s['observation']}}</th>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <button wire:click="$emit('openModal', 'student-modal')" class="form-control w-25 align-self-center mb-4"> Adicionar um aluno </button>
 
 
 </div>
