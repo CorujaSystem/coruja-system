d5a548d3f9f9bf03a6c42ed8fd72c935e84da0ff
Bruno Eduardo <bruno.eduardo@hox.rs>
2022-01-27 10:14:01 -0300

    chore: install chart.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/style.scss', 'public/css')
     .sourceMaps();
+
+mix.copy('node_modules/chart.js/dist/chart.js', 'public/chart.js/chart.js');
